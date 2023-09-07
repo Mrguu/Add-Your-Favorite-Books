30d4db0cd71bdeb3077095957e4ab7180438e0e1
Mr Guu <pyaetheta@gmail.com>
2023-09-07 21:33:52 +0630

    updated backend

--- server/config.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const mysql = require("mysql2");
-
-const urlDB = `mysql://root:25tiUpmNnK5MTejxtLZC@containers-us-west-171.railway.app:5970/railway`;
-
-const connection = mysql.creatConnection(urlDB);
-
-module.exports = connection;
